a4b706c1b27bc9b9ac61ecca4db62ae5fba28a4f
Alex Trzeciak <trebek08@gmail.com>
2017-07-12 01:37:19 -0700

    got base on site

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,25 +4,10 @@ import {Link} from 'react-router';
 export default class App extends Component {
   	render() {
     return (	
-        <div>
-      		<div id='buttons'>
-				<div id='lower_right'>
-					<input className="anchor-image" type="image" src={require("../../static/sprites/hulks.png")}/>
-					<h3 className='title'>Home </h3>
-				</div>
-				<div id='upper_right'>
-					<input className='anchor-image' type="image" src={require("../../static/sprites/tanks3.png")}/>
-					<h3 className='title'>About Me </h3>
-				</div>
-				<div  id='lower_left'>
-					<input className="anchor-image" type="image" src={require("../../static/sprites/robots.png")}/>
-					<h3 className='title'> Projects </h3>
-				</div>
-				<div id='upper_left'>
-					<input className="anchor-image" type="image" src={require("../../static/sprites/brains3.png")}/>
-					<h3 className='title'> Education </h3>
-				</div>
-			</div>
+        <div className="content home">
+        	<div className="textContainer" >
+      		Welcome to My Site! Click around to learn more about me!
+      		</div>
       	 </div>
     );
   }
--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -4,9 +4,39 @@ export default class Wrapper extends Component {
   render() {
     return (
         <div className="app-wrapper">
-	      		<div className="content">
-	      			{this.props.children}
-	      		</div>
+      		<div>
+      			{this.props.children}
+      		</div>
+      		<div id='buttons'>
+				<div id='lower_right'>
+					<input className="anchor-image" type="image" src={require("../../static/sprites/hulks.png")}/>
+					<h3 className='title'>Home </h3>
+				</div>
+				<div id='upper_right'>
+					<input className='anchor-image' type="image" src={require("../../static/sprites/tanks3.png")}/>
+					<h3 className='title'>About Me </h3>
+				</div>
+				<div  id='lower_left'>
+					<input className="anchor-image" type="image" src={require("../../static/sprites/robots.png")}/>
+					<h3 className='title'> Projects </h3>
+				</div>
+				<div id='upper_left'>
+					<input className="anchor-image" type="image" src={require("../../static/sprites/brains3.png")}/>
+					<h3 className='title'> Education </h3>
+				</div>
+			</div>
+			
+			<div id='bottom'>
+				<input id='hero' type="image" src={require("../../static/sprites/hero2.png")}/>
+				<div id="links">
+					<a href="https://www.linkedin.com/in/alextrz" target="_blank" className="space fa fa-linkedin fa-lg"></a>
+					<a href="https://coderbyte.com/profile/trebek1" target="_blank" className="space fa fa-code fa-lg"></a>
+					<a href="https://github.com/trebek1" target="_blank" className="space fa fa-github fa-lg"></a>
+					<br/><br/>
+					<a className="mailto" target="_top" href="mailto:atrzeciak@wustl.edu">Contact Me Here</a>  			
+				</div>
+			</div>
+			
       	</div>
     );
   }
