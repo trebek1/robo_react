c2a6d4769386389c01ed5f02c22dd4749d32533e
Alex Trzeciak <trebek08@gmail.com>
2017-07-12 00:56:09 -0700

    got images loading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,24 @@ export default class App extends Component {
   	render() {
     return (	
         <div>
-      		Something Goes here
+      		<div id='buttons'>
+				<div id='lower_right'>
+					<input className="anchor-image" type="image" src={require("../../static/sprites/hulks.png")}/>
+					<h3 className='title'>Home </h3>
+				</div>
+				<div id='upper_right'>
+					<input className='anchor-image' type="image" src={require("../../static/sprites/tanks3.png")}/>
+					<h3 className='title'>About Me </h3>
+				</div>
+				<div  id='lower_left'>
+					<input className="anchor-image" type="image" src={require("../../static/sprites/robots.png")}/>
+					<h3 className='title'> Projects </h3>
+				</div>
+				<div id='upper_left'>
+					<input className="anchor-image" type="image" src={require("../../static/sprites/brains3.png")}/>
+					<h3 className='title'> Education </h3>
+				</div>
+			</div>
       	 </div>
     );
   }
