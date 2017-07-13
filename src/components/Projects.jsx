6df4d1692c4355d095f05310d6818402e95551e7
Alex Trzeciak <trebek08@gmail.com>
2017-07-12 19:41:16 -0700

    setup the projects selector tab

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,8 +6,16 @@ export default class Projects extends Component {
     return (	
         <div className="content projects">
         	<div className="textContainer">
-      		Projects
-      		</div>
+				<div id='p-list'>
+					<h3 className='section-title'> Projects ---> Click For More Information! </h3>
+					<ul>
+						<li className="projects" onclick='changeDemo(1)' id="tennis-project"> Tennis Courts in SF - July 2017 </li>
+						<li className="projects" onclick='changeDemo(4)' id="itunes-project"> Itunes Sampler - March 2016 </li>
+						<li className="projects" onclick='changeDemo(3)' id="maps-project"> Many Maps - Feb 2016</li>
+						<li className="projects" onclick='changeDemo(2)' id ="scene-project"> Scene in the City - May 2015  </li>
+					</ul>
+				</div>
+      	 	</div>
       	 </div>
     );
   }
