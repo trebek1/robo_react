5baf0cdb3ee4f3945f668f2899c872aa97af427a
Alex Trzeciak <trebek08@gmail.com>
2017-07-12 19:07:20 -0700

    added edu page

--- src/components/Edu.jsx
+++ src/components/Edu.jsx
@@ -6,8 +6,18 @@ export default class Edu extends Component {
     return (	
         <div className="content edu">
         	<div className="textContainer">
-      		Edu
-      		</div>
+      			<h3>GENERAL ASSEMBLY</h3>
+				<div id='edutext'>Web Development Immersive</div>
+				<div id='edutext2'>March 2015 to June 2015</div>
+				
+				<h3>WASHINGTON UNIVERSITY IN ST. LOUIS</h3>
+				<div id='edutext'>Master of Science in Finance</div>
+				<div id='edutext2'>August 2012 to May 2013</div>
+				
+				<h3>BALDWIN WALLACE UNIVERSITY</h3>
+				<div id='edutext'>B.S. Mathematics and Economics</div>
+				<div id='edutext2'>August 2008 to May 2012</div>
+	      		</div>
       	 </div>
     );
   }
