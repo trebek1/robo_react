49162e43e993076686eca90ba42892813c4fb35c
Alex Trzeciak <trebek08@gmail.com>
2017-07-18 01:14:26 -0700

    fixed hover and fixed mobile screen size

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -15,12 +15,12 @@ export default class Projects extends Component {
 					<div className="projectHeader">
 						<h3 className='section-title'> Projects ---> Click For More Information! </h3>
 						<ul>
-							<div><Link className="projects orangeText pj" to="/projects/tennis" id="tennis-project"> Tennis Courts in SF - July 2017 </Link></div>
-							<div><Link className="projects orangeText pj" to="/projects/codeslice" id ="codeslice-project"> Codeslice - May 2017  </Link></div>
-							<div><Link className="projects orangeText pj" to="/projects/workout" id ="workout-project"> Log The Workout - March 2017  </Link></div>
-							<div><Link className="projects orangeText pj" to="/projects/itunes" id="itunes-project"> Itunes Sampler - March 2016 </Link></div>
-							<div><Link className="projects orangeText pj" to="/projects/maps" id="maps-project"> Many Maps - Feb 2016</Link></div>
-							<div><Link className="projects orangeText pj" to="/projects/scene" id ="scene-project"> Scene in the City - May 2015  </Link></div>
+							<div className="pj"><Link className="projects orangeText pj" to="/projects/tennis" id="tennis-project"> Tennis Courts in SF - July 2017 </Link></div>
+							<div className="pj"><Link className="projects orangeText pj" to="/projects/codeslice" id ="codeslice-project"> Codeslice - May 2017  </Link></div>
+							<div className="pj"><Link className="projects orangeText pj" to="/projects/workout" id ="workout-project"> Log The Workout - March 2017  </Link></div>
+							<div className="pj"><Link className="projects orangeText pj" to="/projects/itunes" id="itunes-project"> Itunes Sampler - March 2016 </Link></div>
+							<div className="pj"><Link className="projects orangeText pj" to="/projects/maps" id="maps-project"> Many Maps - Feb 2016</Link></div>
+							<div className="pj"><Link className="projects orangeText pj" to="/projects/scene" id ="scene-project"> Scene in the City - May 2015  </Link></div>
 							
 						</ul>
 					</div>
